e8e53057cc78c89676fbdff24979b357d4d14c9a
summermong <2514o_o@naver.com>
2024-05-06 20:11:34 +0900

    404. 낙관적 업데이트

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { fetchEvent, updateEvent } from '../../util/http.js';
+import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
@@ -12,12 +12,28 @@ export default function EditEvent() {
   const params = useParams();
 
   const { data, isPending, isError, error } = useQuery({
-    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
     queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async data => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ['events', params.id] });
+      const previousEvent = queryClient.getQueryData(['events', params.id]);
+
+      queryClient.setQueryData(['events', params.id], newEvent);
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id]);
+    },
   });
 
   function handleSubmit(formData) {
@@ -46,7 +62,7 @@ export default function EditEvent() {
           title="Failed to load event"
           message={
             error.info?.message ||
-            'Failed to load event. please check your inputs and try again later.'
+            'Failed to load event. Please check your inputs and try again later.'
           }
         />
         <div className="form-actions">
